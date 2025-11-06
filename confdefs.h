d797b4d676d6120a283679a53aed3ec59fbcfb40
Akanksha Hotta <akanksha.hotta@intel.com>
2025-11-06 11:54:20 +0000

    XE-KMD Backport-Release: xebr-v6.17-rc7-250924.0.0.0
    Generation Information:
    XeKMD-Backport-Tag: v6.17-rc7-250924.0
    Backports Tag: xeb_v6.17-rc7-250924.0
    Backports Branch: temp/oot-backport/main
    Backports Head: 7603b0d
    PR-Generated: Auto Backported.
    
    Signed-off-by: Akanksha Hotta <akanksha.hotta@intel.com>

---
+++ confdefs.h
@@ -0,0 +1,9 @@
+/* confdefs.h */
+#define PACKAGE_NAME "intel-xe-drm"
+#define PACKAGE_TARNAME "intel-xe-drm"
+#define PACKAGE_VERSION "1.0"
+#define PACKAGE_STRING "intel-xe-drm 1.0"
+#define PACKAGE_BUGREPORT "linux-graphics@intel.com"
+#define PACKAGE_URL ""
+#define PACKAGE "intel-xe-drm"
+#define VERSION "1.0"
